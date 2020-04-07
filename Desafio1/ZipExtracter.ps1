8bfd1bf9f6623e1e3b7629855a796717799babb0
Fede.Markoo <fede.markoo@gmail.com>
2020-04-06 21:29:13 -0300

    al toke perri

--- Desafio1/ZipExtracter.ps1
+++ Desafio1/ZipExtracter.ps1
@@ -1,80 +1,139 @@
 ﻿cls
-$ErrorActionPreference = "SilentlyContinue"  #para que deje de mostrar mensajes de error : los prints consumen recursos
-$file = "C:\Users\fmarkowicz\Desktop\NovaMas\Desafio1\Desafio2.zip"
-$salida = $file.Substring(0, $file.LastIndexOf(".")) 
 
 
-$elapsed = [System.Diagnostics.Stopwatch]::StartNew()  #contador de tiempo
 
-write-host "Started at $(get-date)"
-$i=0
-$pass = Get-Content .\passwords1.csv
-$file
-$cantXJob = 50
-"tiempo consiguiendo "+$pass.Length +" contraseñas: "+$elapsed.Elapsed.ToString()
-$elapsed = [System.Diagnostics.Stopwatch]::StartNew()  #contador de tiempo
-$jobsNumber = 1;
-$desde = 0;
-while($desde -lt $pass.Length){
-    [array]$copy;
-    $pass.CopyTo($copy,$desde);
-    "creando job $jobsNumber"
-    Start-ThreadJob -ScriptBlock {
-        "corriendo job $jobNumber"
-        $pudoDescomprimir = $false
-        #crear hilos para enviar secciones de 50 contraseñas (cant thread = $pass.Length/50) para mejorar esa performance
-        foreach ($line in $pass){
-            $i++;
-            $cmd = "& .\7z.exe x $file -p$line -o$salida -mmt -aoa"
-            if((Invoke-Expression $cmd)[12].StartsWith("Ever")){  #el mensaje completo es "Everything is ok" pero cuando mas caracteres valida, mas consume aunque al primer fallo deja de comparar
-                $pudoDescomprimir = $true
-                #matas todos los hilos del array
-                break
+    $ErrorActionPreference = "SilentlyContinue"  #para que deje de mostrar mensajes de error : los prints consumen recursos
+    $file = "C:\Users\fmarkowicz\Desktop\NovaMas\Desafio1\Desafio2.zip"
+    $salida = $file.Substring(0, $file.LastIndexOf(".")) 
+
+
+    $elapsed = [System.Diagnostics.Stopwatch]::StartNew()  #contador de tiempo
+
+    write-host "Started at $(get-date)"
+    $i=0
+    $pass = Get-Content .\passwords.csv
+    $file
+    "tiempo consiguiendo "+$pass.Length +" contraseñas: "+$elapsed.Elapsed.ToString()
+    $elapsed = [System.Diagnostics.Stopwatch]::StartNew()  #contador de tiempo
+    $cantXJob = 50
+    $desde = 0;
+
+    # the number of threads
+    $count = [int]($pass.Length/$cantXJob)
+
+    $dir = (dir)[0].DirectoryName
+
+    
+# the pool will manage the parallel execution
+$pool = [RunspaceFactory]::CreateRunspacePool(1, $count)
+
+try {    
+    $pool.Open()
+
+
+
+    # create and run the jobs to be run in parallel
+    $jobs = New-Object object[] $count
+ 
+    "Creando $count jobs"
+    for ($jobsNumber = 0; $jobsNumber -lt $count; $jobsNumber++) {
+        $ps = [PowerShell]::Create()
+        $ps.RunspacePool = $pool
+       
+        # add the script block to run
+         [void]$ps.AddScript({
+            param($pass, $jobNumber, $file, $salida, $cantXJob, $dir, $jobs)
+            
+            $ErrorActionPreference = "SilentlyContinue"  #para que deje de mostrar mensajes de error : los prints consumen recursos
+    
+            $dir="C:\tempUnZiper$jobNumber"
+
+            New-Item -Path $dir -ItemType Directory | %{$_.Attributes = "hidden"}
+            #attrib +h "tempUnZiper$jobNumber" 
+
+            $pudoDescomprimir = $false
+            $i=0
+            
+            $init = "& C:\Users\fmarkowicz\Desktop\NovaMas\Desafio1\7z.exe x $file -p"
+            
+            $end = " -o$dir -mmt -aoa"
+
+            #crear hilos para enviar secciones de 50 contraseñas (cant thread = $pass.Length/50) para mejorar esa performance
+            foreach ($line in $pass){
+                $i++;
+
+                $cmd = $init + $line + $end
+                
+                $result = Invoke-Expression $cmd
+                if(($result)[12].StartsWith("Ever")){  #el mensaje completo es "Everything is ok" pero cuando mas caracteres valida, mas consume aunque al primer fallo deja de comparar
+                    $pudoDescomprimir = $true
+                    "ok"
+                    $line
+                    break
+                }
+                if($i -ge $cantXJob){
+                    "false"
+                    break
+                }
             }
-            if($i -eq 50){
-                break
+            if($pudoDescomprimir){
+                $index = $salida.LastIndexOf("\");
+                $name = $salida.Substring($index+1)
+                $salida = $salida.Substring(0,$index)
+                $salida
+                Remove-Item "$salida\$name" -Force -Recurse
+                Rename-Item -Path $dir -NewName "$name"
+                attrib -h "C:\$name"                
+                Move-Item -path "C:\$name" -Destination $salida 
             }
-        }
-        if($pudoDescomprimir){
-            "Se probaron $i contraseñas en el job numero $jobNumber, siendo $line la correcta"
-        }else{
-            "Se probaron $i contraseñas en el job numero $jobNumber sin exito"
-        }
+        })
 
-        return $pudoDescomprimir
-    } -ThrottleLimit 2  |Wait-Job
-    $desde = $cantXJob * $jobsNumber 
-    $jobsNumber++;
+        
+        $desde = $cantXJob * $jobsNumber
+        $hasta = $desde+$cantXJob-1
+        $copy = $pass[$desde..$hasta]
+
+
+        # optional: add parameters
+        [void]$ps.AddParameter("pass", $copy)
+        [void]$ps.AddParameter("jobNumber", $jobsNumber)
+        [void]$ps.AddParameter("file", $file)
+        [void]$ps.AddParameter("salida", $salida)
+        [void]$ps.AddParameter("cantXJob", $cantXJob)
+        [void]$ps.AddParameter("dir", $dir)
+        [void]$ps.AddParameter("jobs", $jobs)
+
+
+        # start async execution
+        $jobs[$jobsNumber] = [PSCustomObject]@{
+            PowerShell = $ps
+            AsyncResult = $ps.BeginInvoke()
+        }
+    }
+
+    $isCompleted = $false
+    while(-not $isCompleted){
+        foreach ($job in $jobs) {
+            try {
+                if($job.AsyncResult.IsCompleted){
+                    $result = $job.PowerShell.EndInvoke($job.AsyncResult)
+                    $isCompleted = $result[0].StartsWith("ok")
+                }
+                if($isCompleted){
+                    $jobNum = $job.PowerShell.Commands.Commands.parameters.GetValue(1).value
+                    "Se logro descomprimir el archivo en el Job $jobNum con contraseña "+$result[1]
+                    break
+                }
+            }catch {}
+        }
+    }
+    $elapsed.Elapsed.ToString()
+    foreach ($job in $jobs) {
+        $job.PowerShell.Dispose()
+        Remove-Item "C:\tempUnZiper*" -Force -Recurse
+    }
 }
-
+finally {
+    $pool.Dispose()
+}
 $elapsed.Elapsed.ToString()
-
-
-
-
-
-
-function unZip($pass, $jobNumber){
-"corriendo job $jobNumber"
-    $pudoDescomprimir = $false
-    #crear hilos para enviar secciones de 50 contraseñas (cant thread = $pass.Length/50) para mejorar esa performance
-    foreach ($line in $pass){
-        $i++;
-        $cmd = "& .\7z.exe x $file -p$line -o$salida -mmt -aoa"
-        if((Invoke-Expression $cmd)[12].StartsWith("Ever")){  #el mensaje completo es "Everything is ok" pero cuando mas caracteres valida, mas consume aunque al primer fallo deja de comparar
-            $pudoDescomprimir = $true
-            #matas todos los hilos del array
-            break
-        }
-        if($i -eq 50){
-            break
-        }
-    }
-    if($pudoDescomprimir){
-        "Se probaron $i contraseñas en el job numero $jobNumber, siendo $line la correcta"
-    }else{
-        "Se probaron $i contraseñas en el job numero $jobNumber sin exito"
-    }
-
-    return $pudoDescomprimir
-}
