1da85b2d60d6488509ad6a1b9dbbc19237acf0d6
Fede.Markoo <fede.markoo@gmail.com>
2020-04-07 00:13:48 -0300

    chupala gato

--- Desafio1/ZipExtracter.ps1
+++ Desafio1/ZipExtracter.ps1
@@ -3,7 +3,7 @@
 
 
     $ErrorActionPreference = "SilentlyContinue"  #para que deje de mostrar mensajes de error : los prints consumen recursos
-    $file = "C:\Users\fmarkowicz\Desktop\NovaMas\Desafio1\Desafio2.zip"
+    $file = "C:\Users\fmarkowicz\Desktop\NovaMas\Desafio1\Archivo#S1.zip"
     $salida = $file.Substring(0, $file.LastIndexOf(".")) 
 
 
@@ -11,15 +11,16 @@
 
     write-host "Started at $(get-date)"
     $i=0
-    $pass = Get-Content .\passwords.csv
+    $pass = Get-Content .\passwords-1M.csv # | Sort-Object {Get-Random}
     $file
     "tiempo consiguiendo "+$pass.Length +" contraseñas: "+$elapsed.Elapsed.ToString()
     $elapsed = [System.Diagnostics.Stopwatch]::StartNew()  #contador de tiempo
-    $cantXJob = 50
+   
     $desde = 0;
 
     # the number of threads
-    $count = [int]($pass.Length/$cantXJob)
+    $count = 3
+    $cantXJob = [int]($pass.Length/$count)+1
 
     $dir = (dir)[0].DirectoryName
 
@@ -35,13 +36,13 @@ try {
     # create and run the jobs to be run in parallel
     $jobs = New-Object object[] $count
  
-    "Creando $count jobs"
+    "Creando "+($count)+" jobs de $cantXJob palabras"
     for ($jobsNumber = 0; $jobsNumber -lt $count; $jobsNumber++) {
         $ps = [PowerShell]::Create()
         $ps.RunspacePool = $pool
        
         # add the script block to run
-         [void]$ps.AddScript({
+        [void]$ps.AddScript({
             param($pass, $jobNumber, $file, $salida, $cantXJob, $dir, $jobs)
             
             $ErrorActionPreference = "SilentlyContinue"  #para que deje de mostrar mensajes de error : los prints consumen recursos
@@ -57,13 +58,11 @@ try {
             $init = "& C:\Users\fmarkowicz\Desktop\NovaMas\Desafio1\7z.exe x $file -p"
             
             $end = " -o$dir -mmt -aoa"
-
             #crear hilos para enviar secciones de 50 contraseñas (cant thread = $pass.Length/50) para mejorar esa performance
             foreach ($line in $pass){
                 $i++;
-
-                $cmd = $init + $line + $end
                 
+                $cmd = $init + $line + $end
                 $result = Invoke-Expression $cmd
                 if(($result)[12].StartsWith("Ever")){  #el mensaje completo es "Everything is ok" pero cuando mas caracteres valida, mas consume aunque al primer fallo deja de comparar
                     $pudoDescomprimir = $true
@@ -80,7 +79,6 @@ try {
                 $index = $salida.LastIndexOf("\");
                 $name = $salida.Substring($index+1)
                 $salida = $salida.Substring(0,$index)
-                $salida
                 Remove-Item "$salida\$name" -Force -Recurse
                 Rename-Item -Path $dir -NewName "$name"
                 attrib -h "C:\$name"                
@@ -111,13 +109,18 @@ try {
         }
     }
 
+    $isAllCompl = $false
     $isCompleted = $false
-    while(-not $isCompleted){
+    while(-not $isCompleted -and -not $isAllCompl ){
+        sleep -Milliseconds 500
+        $isAllCompl = $true
         foreach ($job in $jobs) {
             try {
                 if($job.AsyncResult.IsCompleted){
                     $result = $job.PowerShell.EndInvoke($job.AsyncResult)
                     $isCompleted = $result[0].StartsWith("ok")
+                }else{
+                    $isAllCompl = $false
                 }
                 if($isCompleted){
                     $jobNum = $job.PowerShell.Commands.Commands.parameters.GetValue(1).value
@@ -130,8 +133,8 @@ try {
     $elapsed.Elapsed.ToString()
     foreach ($job in $jobs) {
         $job.PowerShell.Dispose()
-        Remove-Item "C:\tempUnZiper*" -Force -Recurse
     }
+    Remove-Item "C:\tempUnZiper*" -Force -Recurse
 }
 finally {
     $pool.Dispose()
